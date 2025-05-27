1209ea49b6640924f85055759a6130bf1de4b9cd
Eduardo Belchior <65121687+Educhior@users.noreply.github.com>
2025-05-27 13:09:25 +0000

    Adiciona a função printList para exibir a lista duplamente encadeada

--- Aula_04.cpp
+++ Aula_04.cpp
@@ -9,13 +9,28 @@ struct Node{ // Definindo a estrutura do nó
 };
 
 Node* build(); // Função para construir a lista duplamente encadeada
+void printList(Node* head);
 
 int main() {
     cout << "Hello, World!" << endl;
     cout << endl;
     Node* head = build(); // Construindo a lista duplamente encadeada
+    printList(head); // Imprimindo a lista
 
     return 0;
+}
+
+void printList(Node* L) {
+    cout << "L "; // Imprimindo a lista
+    while (L != nullptr) { // Enquanto houver nós na lista
+        if (L->prev == nullptr) {
+            cout << "-|> h"; // Imprime se o nó anterior for nulo
+        } else {
+            cout << " <=> " << L->data; // Imprime o valor do nó atual
+        }
+        L = L->next; // Avança para o próximo nó
+    }
+    cout << "-> NULL" << endl;
 }
 
 Node* build() {
@@ -24,6 +39,35 @@ Node* build() {
     head->prev = nullptr; // Inicializando o nó anterior como nulo
 
     Node* n1 = new Node; // Criando o segundo nó
+    head->next = n1; // Ligando o primeiro nó ao segundo
+    n1->prev = head; // Ligando o segundo nó ao primeiro
+    n1->data = 1; // Atribuindo valor ao segundo nó
 
+    Node* n2 = new Node; 
+    n1->next = n2; 
+    n2->prev = n1; 
+    n2->data = 2;
+
+    Node* n3 = new Node;
+    n2->next = n3;
+    n3->prev = n2;
+    n3->data = 3;
+
+    Node* n4 = new Node;
+    n3->next = n4;
+    n4->prev = n3;
+    n4->data = 4;
+
+    Node* n5 = new Node;
+    n4->next = n5;
+    n5->prev = n4;
+    n5->data = 5;
+
+    Node* n6 = new Node;
+    n5->next = n6;
+    n6->prev = n5;
+    n6->data = 6;
+    n6->next = nullptr; // O último nó aponta para nulo
+    
     return head;
 }
